00dd665a53e94aca5023bdf2b8f37a7caa76aaa0
Nicolette Lee <nicolettelee94@gmail.com>
2021-06-03 19:08:28 -0400

    First commit

---
+++ scripts/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask
+
+def create_app():
+
+	app = Flask(__name__)
+
+	#app.config.from_pyfile("config.py")
+	app.config.from_mapping(
+		#DATABASE=os.path.join(app.instance_path, "chars.sqlite")
+		DATABASE="scripts/chars.sqlite"
+	)
+
+	@app.route("/")
+	def factory_hi():
+		return("Made from app factory")
+
+	from . import database
+	database.init_app(app)
+
+	return app
