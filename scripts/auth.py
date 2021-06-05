7ef3441223776c58afdfb1de29dd4af08efe1753
Nicolette Lee <nicolettelee94@gmail.com>
2021-06-05 14:16:46 -0400

    Began adding login functionality; also updated registration form to censor password and to implement minimum length requirement

--- scripts/auth.py
+++ scripts/auth.py
@@ -13,11 +13,12 @@ def register():
 		username = request.form["username"]
 		password = request.form["password"]
 
-
 		db = get_db()
 		error = None
 
 		invalid_chars = ["!", "?", "#", "$", "%", "&", "^", "*"]
+		# change later to use regex: check for any invalid chars in username
+		# also ensure that at least one alphabetic, numeric, and non-alnum are in password
 
 		if not username:
 			error = "Registration error: Username is required."
@@ -25,6 +26,8 @@ def register():
 			error = "Registration error: Password is required."
 		elif any(invalid_char in username for invalid_char in invalid_chars):
 			error = f"Username cannot contain any of the following characters: {','.join(invalid_chars)}"
+		elif len(password) < 8:
+			error = "Error: Password must be at least 8 characters in length."
 		elif db.execute(
 			"SELECT id from users WHERE username = ?", (username,)
 		).fetchone() is not None:
@@ -43,3 +46,28 @@ def register():
 			flash(error)
 
 	return render_template("register.html")
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+
+	if request.method == "POST":
+		username = request.form["username"]
+		password = request.form["password"]
+
+		db = get_db()
+		error = None
+		user_lookup = db.execute("SELECT * from users WHERE username = ?", (username,)).fetchone()
+
+		if not username:
+			error = "Login error: Username is required."
+		elif not password:
+			error = "Login error: Password is required."
+		elif not check_password_hash(user_lookup["password"], password):
+			error = f"Login error: Password is incorrect."
+
+		if error is None:
+			flash("Login successful.")
+		else:
+			flash(error)
+
+	return render_template("login.html")
